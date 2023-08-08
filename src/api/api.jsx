fdea995c90f458649b2fef80a194a89b6b32a7e6
seonik <ikik.pd@gmail.com>
2023-08-08 22:51:06 +0900

    refactor. 공공데이터 API key env 수정

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
 export async function fetchData() {
-  const url = 'http://apis.data.go.kr/1543061/abandonmentPublicSrvc/sido';
+  const url = 'http://apis.data.go.kr/1543061/abandonmentPublicSrvc/kind?up_kind_cd=417000';
   const queryParams =
-    '?' +
+    '&' +
     encodeURIComponent('serviceKey') +
     '=' +
-    encodeURIComponent('9RFjRc/Z+k7QyU9ji73gFB3PKb3VFuioIAN/S7dm2MbcvfODYwaJrRiEnFroeMh2C3TOTgfpEHL6xnLB2tcV9g==') +
+    encodeURIComponent(process.env.REACT_APP_DATA_API_KEY) +
+    '&' +
+    encodeURIComponent('json') +
     '&' +
     encodeURIComponent('numOfRows') +
     '=' +
