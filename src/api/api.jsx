3270f96e7f3db343b7630fd22b5bc12933a10ce9
jinheepark <jhp202@naver.com>
2023-08-09 16:45:22 +0900

    add animal album feature

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,25 +1,98 @@
 import axios from 'axios';
+// import { response } from 'express';
 
-export async function fetchData() {
-  const url = 'http://apis.data.go.kr/1543061/abandonmentPublicSrvc/kind?up_kind_cd=417000';
+/*
+ * 오픈API (유기동물 조회)
+ * URL : http://apis.data.go.kr
+ *
+ * 1) [시/도]
+ * 2) [시/군/구]
+ * 3) [보호소]
+ * 4) [품종]
+ * 5) [유기동물]
+ */
+
+const DATA_API_KEY =
+  '&' +
+  encodeURIComponent('serviceKey') +
+  '=' +
+  encodeURIComponent(process.env.REACT_APP_DATA_API_KEY) +
+  '&' +
+  encodeURIComponent('_type') +
+  '=' +
+  encodeURIComponent('json');
+
+const BASE_URL = 'http://apis.data.go.kr/1543061/abandonmentPublicSrvc';
+
+/** 1) [시/도] - 오픈API 유기동물 데이터 조회  */
+export async function SidoApi() {
+  const url = `${BASE_URL}/sido`;
   const queryParams =
-    '&' +
-    encodeURIComponent('serviceKey') +
+    '?' +
+    encodeURIComponent('numOfRows') +
     '=' +
-    encodeURIComponent(process.env.REACT_APP_DATA_API_KEY) +
-    '&' +
-    encodeURIComponent('json') +
-    '&' +
-    encodeURIComponent('json') +
-    '=' +
-    encodeURIComponent('3') +
+    encodeURIComponent(17) +
     '&' +
     encodeURIComponent('pageNo') +
     '=' +
-    encodeURIComponent('1');
+    encodeURIComponent(1) +
+    DATA_API_KEY;
 
   const response = await axios.get(url + queryParams);
-  console.log('response data : ', response);
+  console.log(response);
+  return response.data;
+}
 
+/** 2) [시/군/구] - 오픈API 유기동물 데이터 조회  */
+export async function SigunguApi() {
+  const url = `${BASE_URL}/sigungu`;
+  const queryParams = '?' + encodeURIComponent('upr_cd') + '=' + encodeURIComponent(6110000) + DATA_API_KEY;
+
+  const response = await axios.get(url + queryParams);
+  return response.data;
+}
+
+/** 3) [보호소] - 오픈API 유기동물 데이터 조회  */
+export async function ShelterApi() {
+  const url = `${BASE_URL}/shelter`;
+  const queryParams =
+    '?' +
+    encodeURIComponent('upr_cd') +
+    '=' +
+    encodeURIComponent(6110000) +
+    '&' +
+    encodeURIComponent('org_cd') +
+    '=' +
+    encodeURIComponent(3220000) +
+    DATA_API_KEY;
+
+  const response = await axios.get(url + queryParams);
+  return response.data;
+}
+
+/** 4) [품종] - 오픈API 유기동물 데이터 조회  */
+export async function AnimalKindApi() {
+  const url = `${BASE_URL}/kind`;
+  const queryParams = '?' + encodeURIComponent('up_kind_cd') + '=' + encodeURIComponent(417000) + DATA_API_KEY;
+
+  const response = await axios.get(url + queryParams);
+  return response.data;
+}
+
+/** 5) [유기동물] - 오픈API 유기동물 데이터 조회  */
+export async function AnimalApi() {
+  const url = `${BASE_URL}/abandonmentPublic`;
+  const queryParams =
+    '?' +
+    encodeURIComponent('bgnde') +
+    '=' +
+    encodeURIComponent(20230801) +
+    '&' +
+    encodeURIComponent('endde') +
+    '=' +
+    encodeURIComponent(20230808) +
+    DATA_API_KEY;
+
+  const response = await axios.get(url + queryParams);
   return response.data;
 }
--- src/pages/pets/Pets.jsx
+++ src/pages/pets/Pets.jsx
@@ -1,8 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
-import { fetchData } from '../../api/api';
+import { AnimalKindApi, AnimalApi, SidoApi, SigunguApi } from '../../api/api';
 
 function Pets() {
-  const { data, status } = useQuery(['fetchData'], fetchData);
+  const { data, status } = useQuery(['animalData'], AnimalApi);
 
   if (status === 'loading') {
     return <p>Loading...</p>;
@@ -12,10 +12,31 @@ function Pets() {
     return <p>Error fetching data</p>;
   }
 
+  const animalItems = data.response.body.items.item; // Access the array of animal items
+
   return (
     <div>
-      <p>Status: {status}</p>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <h1>동물친구들을 소개합니다.</h1>
+      <div className="album-container">
+        {animalItems.map((animal) => (
+          <div key={animal.desertionNo} className="animal-card">
+            <img className="animal-image" src={animal.popfile} alt={`Animal ${animal.desertionNo}`} />
+            <p>
+              <strong>Kind:</strong> {animal.kindCd}
+            </p>
+            <p>
+              <strong>Color:</strong> {animal.colorCd}
+            </p>
+            <p>
+              <strong>Age:</strong> {animal.age}
+            </p>
+            <p>
+              <strong>Happen Place:</strong> {animal.happenPlace}
+            </p>
+            {/* Add more information as needed */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
